c64ddf1919f92e4f7c5c35d8ad31bc948970c0f3
artem <seonetartem@gmail.com>
2013-12-10 17:19:18 +0200

    acl add stratege

--- config/module.config.php
+++ config/module.config.php
@@ -4,6 +4,7 @@ return array(
     'service_manager' => array(
         'invokables' => array(
             'AccessDeniedStrategy' => 'Zf2Acl\Mvc\View\Http\AccessDeniedStrategy',
+            'GuestRedirectUserSetHttpCode' => 'Zf2Acl\Mvc\View\Http\GuestRedirectUserSetHttpCode',
             'AclListener'          => 'Zf2Acl\Permissions\Acl\AclListener',
             'Acl'                  => 'Zf2Acl\Permissions\Acl\Acl',
         ),
